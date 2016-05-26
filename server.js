6daff84cbe80149511606547469f97733e7888c4
giriganapathy <giriganapathy@gmail.com>
2016-05-26 17:10:39 +0530

    sixth commit

--- server.js
+++ server.js
@@ -51,8 +51,8 @@ dialog.on("intent.pnr.enquiry", [
 ]);
 //bot.listenStdin();
 var server = restify.createServer();
-//server.use(bot.verifyBotFramework({ appId: process.env.appId, appSecret: process.env.appSecret }));
-server.use(bot.verifyBotFramework());
+server.use(bot.verifyBotFramework({ appId: process.env.appId, appSecret: process.env.appSecret }));
+//server.use(bot.verifyBotFramework());
 server.post("/api/messages", bot.listen());
 server.listen(process.env.port, function () {
     console.log("%s listening to %s", server.name, server.url);
