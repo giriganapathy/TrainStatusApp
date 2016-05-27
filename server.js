b2db2add2d8a2cb1409b2a4f927614d7cd4b4b75
giriganapathy <giriganapathy@gmail.com>
2016-05-27 16:19:44 +0530

    fourteen commit

--- server.js
+++ server.js
@@ -7,13 +7,14 @@ This is a sample bot.
 var restify = require("restify");
 var builder = require("botbuilder");
 var model = process.env.model || "https://api.projectoxford.ai/luis/v1/application?id=83b0d263-bcb7-4ded-b197-95b25ee68030&subscription-key=b27a7109bc1046fb9cc7cfa874e3f819&q=";
-var key = "embct6154";
+
 var dialog = new builder.LuisDialog(model);
 var bot = new builder.BotConnectorBot(); //new builder.TextBot();
 bot.add("/", dialog);
 dialog.onDefault(builder.DialogAction.send("I'm sorry. I didn't understand."));
 dialog.on("intent.pnr.enquiry", [
     function (session, args) {
+        var key = "embct6154";
         var entity = builder.EntityRecognizer.findEntity(args.entities, 'pnr-number');
         if (null != entity) {
             var pnrNumber = entity.entity;
@@ -25,7 +26,6 @@ dialog.on("intent.pnr.enquiry", [
                 var options = {
                     headers: { "Content-Type": "application/json" }
                 };
-                session.send(entity.entity + "<>" + pnrNumber + "<>" + session.userData.pnrNumber);
                 client.get("http://api.railwayapi.com/pnr_status/pnr/" + session.userData.pnrNumber + "/apikey/" + key, options, function (data, response) {
                     // parsed response body as js object 
                     if (data) {
