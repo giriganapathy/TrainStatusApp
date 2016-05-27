57eff864804716eafe91f54bbacb3bc9d8bd214e
giriganapathy <giriganapathy@gmail.com>
2016-05-27 23:19:46 +0530

    twenty one commit

--- server.js
+++ server.js
@@ -29,9 +29,9 @@ dialog.on("intent.pnr.enquiry", [
                  
                 var req = client.get("http://api.railwayapi.com/pnr_status/pnr/" + session.userData.pnrNumber + "/apikey/" + key + "/", options, function (data, response) {
                     // parsed response body as js object 
-                    session.send("http://api.railwayapi.com/pnr_status/pnr/" + session.userData.pnrNumber + "/apikey/" + key + "\nResponse:" + response + "\nData:" + data);
+                    //session.send("http://api.railwayapi.com/pnr_status/pnr/" + session.userData.pnrNumber + "/apikey/" + key + "\nResponse:" + response + "\nData:" + data);
                     if (data) {
-                        session.send(data["response_code"]);
+                        //session.send(data["response_code"]);
                         var resultInfo = "\nTrain Name: " + data["train_name"] +
                             "\nFrom Station: " + data["from_station"]["name"] +
                             "\To Station: " + data["to_station"]["name"] +
